061493cbb42e6bd7f1c6c5bbd153382f2d6bc048
Mehran Sadeghi Lahijani <mehran.sadeghi.lahijani@gmail.com>
2019-04-15 11:45:29 -0400

    Database is created with the empty StockData table

--- stock.py
+++ stock.py
@@ -11,16 +11,111 @@ This class must:
 	– The class must have a method for initialization of its objects and any other methods you feel are
 	necessary.
 '''
+
+
+	def pull110ItemsURL(self):
+	    # Set up Chrome instance of this url
+	    driver = webdriver.Chrome(executable_path='./chromedriver')
+	    driver.get('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQrender=download')
+
+	    # Click on the 150 option in page size select so we can get all the symbols we need
+	    page_size_select = driver.find_element_by_id('main_content_lstpagesize')
+	    for page_size_option in page_size_select.find_elements_by_tag_name('option'):
+	        if page_size_option.text == '150 Items Per Page':
+	            page_size_option.click()
+	            break
+
+	    return driver.current_url
+
+
+
 	def save_tickers(self, n, url, file_name):
+		if int(n) > 110:
+        	raise Exception("You need to give me a number less than or equal to 150!")
 
+	    # Create request with 150 item url
+	    request = requests.get(url=pull110ItemsURL())
+	    parser = PyQuery(request.text)
+	    table = parser("#CompanylistResults")
 
+	    table_parser = PyQuery(table)
+	    symbols = table_parser("h3")
+	    symbol_list = [symbol for symbol in symbols.text().split()]
+
+	    for i, ticker in enumerate(symbol_list):
+	        try:
+	            if i <= int(n):
+	                Stock(symbol=ticker).price()
+	            else:
+	                break
+	        except:
+	            symbol_list.remove(ticker)
+
+	    f = open(filename, "w")
+	    for i, symbol in enumerate(symbol_list):
+	        if i <= int(n):
+	            f.write(symbol + '\n')
+	        else:
+	            break
+
+	    f.close()
 
 
 	def __init__(self):
+		pass
+		#TODO: How to write appropriate inits?
 		
-
+import sys
+import csv
+import datetime
+import time
+from iex import Stock
 
 class Fetcher:
+
+
+
+
+	def write_to_file(self, ticker, info_writer, endTime):
+	    currentDT = datetime.datetime.now()
+	    hour = str(currentDT.hour)
+	    minute = currentDT.minute
+	    if minute < 10:
+	        minute = '0' + str(minute)
+	    else:
+	        minute = str(minute)
+
+	    # If function exceeds its endTime then exit the module
+	    if currentDT >= endTime:
+	        return
+
+	    ticker_info = Stock(symbol=ticker).quote()
+	    info_writer.writerow([hour + ':' + minute, ticker, ticker_info['low'], ticker_info['high'], ticker_info['open'],
+	                          ticker_info['close'], ticker_info['latestPrice'], ticker_info['latestVolume']])
+
+
+	def fetch_all_data(self, time_lim, ticker_file, csv_file):
+	    open_csv = open(csv_file, 'a')
+	    info_writer = csv.writer(open_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+	    while True:
+	        fp = open(ticker_file)
+	        # Calculate time to sleep until next minute starts
+	        sleepTime = 60 - (datetime.datetime.now().second + datetime.datetime.now().microsecond / 1000000.0)
+	        time.sleep(sleepTime)
+
+	        # Calculate how long the function should run
+	        endTime = datetime.datetime.now() + datetime.timedelta(seconds=int(time_lim))
+	        for ticker in fp:
+	            writeToFile(ticker=ticker.strip('\n'), info_writer=info_writer, endTime=endTime)
+	        fp.close()
+
+
+	def update_ticker(self, ticker):
+		pass
+	def __init__(self):
+		pass
+
 
 '''
 	– Read all the tickers from an input file (tickers.txt) or use some function of the Tickers class to fetch
